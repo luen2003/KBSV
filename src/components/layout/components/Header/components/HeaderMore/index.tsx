d902228357350e6d0089dfa970c386c9232f477c
21020776TaDinhLuong <21020776@vnu.edu.vn>
2025-03-01 07:51:32 +0700

    complete app

--- src/components/layout/components/Header/components/HeaderMore/index.tsx
+++ src/components/layout/components/Header/components/HeaderMore/index.tsx
@@ -4,7 +4,7 @@ import { Notification } from "./components/Notification";
 
 export default function HeaderMore() {
   return (
-    <div className="py-1 pl-4 flex overflow-auto">
+    <div className="py-1 pl-4 flex">
       <Notification />
       <UserInfoHeader />
       <Lang />
