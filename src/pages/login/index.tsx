e46affe5abf6ceb0d89737404061003d2cf16cec
21020776TaDinhLuong <21020776@vnu.edu.vn>
2025-03-02 17:47:03 +0700

    complete app

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -101,114 +101,115 @@ const Login = () => {
 
   return (
     <div className="overflow-hidden flex items-center h-full relative">
-      <div>
-        <img src="/login/login.png" />
-      </div>
-      <div className="flex flex-col flex-1 justify-between h-full">
-        <BlockUi
-          blocking={loading}
-          className="h-full flex items-center justify-center flex-1"
-          loader={<Spinner />}
-        >
-          <div className="w-3/5">
-            <div className="flex flex-col flex-1 text-center mt-20 gap-2">
-              <div>
-                <h1 className="font-bold text-3xl text-gray-014">KB</h1>
-              </div>
-              <div className="pt-50px px-50px">
-                <Formik
-                  initialValues={initValue}
-                  validationSchema={validationSchema}
-                  onSubmit={(values: IFormLoginValue, { setSubmitting }) => {
-                    handleLogin(values.username, values.password);
-                    setSubmitting(false);
-                  }}
-                >
-                  {({
-                    values,
-                    errors,
-                    touched,
-                    handleChange,
-                    handleBlur,
-                    handleSubmit,
-                    isSubmitting,
-                    isValid
-                  }) => (
-                    <Form onSubmit={handleSubmit}>
-                      <div className="">
-                        <div className="flex flex-col items-start">
-                          <div className="flex w-full items-center pl-4 pr-2.5 py-2 rounded text-14px bg-transparent border border-gray-010 text-gray-012 placeholder-gray-012">
-                            <Profile className="w-18px h-18px mr-1.5" />
-                            <input
-                              placeholder={t("login:userNamePlaceHoder")}
-                              className="w-full flex-1 outline-none bg-transparent text-14px resize-none "
-                              type="username"
-                              name="username"
-                              onChange={handleChange}
-                              onBlur={handleBlur}
-                              value={values.username}
-                            />
-                          </div>
-                          <ErrorMessage
-                            className="text-color-red"
-                            name="username"
-                            component="div"
-                          />
-                          <div className="mt-16px flex w-full items-center pl-4 pr-2.5 py-2 rounded text-14px bg-transparent border border-gray-010 text-gray-012 placeholder-gray-012">
-                            <Lock1 className="w-18px h-18px mr-1.5" />
-                            <input
-                              placeholder={t("login:passwordPlaceHoder")}
-                              className="w-full flex-1 outline-none bg-transparent text-14px resize-none"
-                              type={showPassword ? "text" : "password"}
-                              name="password"
-                              onChange={handleChange}
-                              onBlur={handleBlur}
-                              value={values.password}
-                            />
-                            <div onClick={handleShowHideClick}>
-                              {showPassword ? (
-                                <Eye className="w-18px h-18px ml-1.5" />
-                              ) : (
-                                <EyeSlash className="w-18px h-18px ml-1.5" />
-                              )}
-                            </div>
-                          </div>
-                          <ErrorMessage
-                            className="text-color-red"
-                            name="password"
-                            component="div"
-                          />
+  <div className="login-image">
+    <img src="/login/login.png" />
+  </div>
+  <div className="flex flex-col flex-1 justify-between h-full">
+    <BlockUi
+      blocking={loading}
+      className="h-full flex items-center justify-center flex-1"
+      loader={<Spinner />}
+    >
+      <div className="w-3/5 login-form">
+        <div className="form-wrapper flex flex-col flex-1 text-center mt-20 gap-2">
+          <div>
+            <h1 className="font-bold text-3xl text-gray-014">KB</h1>
+          </div>
+          <div className="pt-50px px-50px">
+            <Formik
+              initialValues={initValue}
+              validationSchema={validationSchema}
+              onSubmit={(values: IFormLoginValue, { setSubmitting }) => {
+                handleLogin(values.username, values.password);
+                setSubmitting(false);
+              }}
+            >
+              {({
+                values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting,
+                isValid
+              }) => (
+                <Form onSubmit={handleSubmit}>
+                  <div className="">
+                    <div className="flex flex-col items-start">
+                      <div className="flex w-full items-center pl-4 pr-2.5 py-2 rounded text-14px bg-transparent border border-gray-010 text-gray-012 placeholder-gray-012">
+                        <Profile className="w-18px h-18px mr-1.5" />
+                        <input
+                          placeholder={t("login:userNamePlaceHoder")}
+                          className="w-full flex-1 outline-none bg-transparent text-14px resize-none "
+                          type="username"
+                          name="username"
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          value={values.username}
+                        />
+                      </div>
+                      <ErrorMessage
+                        className="text-color-red"
+                        name="username"
+                        component="div"
+                      />
+                      <div className="mt-16px flex w-full items-center pl-4 pr-2.5 py-2 rounded text-14px bg-transparent border border-gray-010 text-gray-012 placeholder-gray-012">
+                        <Lock1 className="w-18px h-18px mr-1.5" />
+                        <input
+                          placeholder={t("login:passwordPlaceHoder")}
+                          className="w-full flex-1 outline-none bg-transparent text-14px resize-none"
+                          type={showPassword ? "text" : "password"}
+                          name="password"
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          value={values.password}
+                        />
+                        <div onClick={handleShowHideClick}>
+                          {showPassword ? (
+                            <Eye className="w-18px h-18px ml-1.5" />
+                          ) : (
+                            <EyeSlash className="w-18px h-18px ml-1.5" />
+                          )}
                         </div>
                       </div>
-                      <div className="flex justify-center">
-                        <button
-                          className="w-full h-38px mt-16px bg-color-primary text-color-Textbutton rounded hover:text-gray-013 font-medium"
-                          type="submit"
-                        >
-                          {`${t("login:login")}`}
-                        </button>
-                      </div>
-                    </Form>
-                  )}
-                </Formik>
-              </div>
-              <div className="mt-12px mb-50px flex flex-col items-center text-gray-014">
-                <span>--Or--</span>
-                <div
-                  className="cursor-pointer border border-gray-010 w-fit px-3 py-1"
-                  onClick={() => {}}
-                >
-                  KBS SSO
-                </div>
-              </div>
+                      <ErrorMessage
+                        className="text-color-red"
+                        name="password"
+                        component="div"
+                      />
+                    </div>
+                  </div>
+                  <div className="flex justify-center">
+                    <button
+                      className="w-full h-38px mt-16px bg-color-primary text-color-Textbutton rounded hover:text-gray-013 font-medium"
+                      type="submit"
+                    >
+                      {`${t("login:login")}`}
+                    </button>
+                  </div>
+                </Form>
+              )}
+            </Formik>
+          </div>
+          <div className="mt-12px mb-50px flex flex-col items-center text-gray-014">
+            <span>--Or--</span>
+            <div
+              className="cursor-pointer border border-gray-010 w-fit px-3 py-1"
+              onClick={() => {}}
+            >
+              KBS SSO
             </div>
           </div>
-        </BlockUi>
-        <div className="text-center text-14px mb-2">
-          Copyright@KBSecurities 2022 | Privacy Policy
         </div>
       </div>
+    </BlockUi>
+    <div className="text-center text-14px mb-2">
+      Copyright@KBSecurities 2022 | Privacy Policy
     </div>
+  </div>
+</div>
+
   );
 };
 
